92806bd27a5decfb6ea117a374ced333dc87048d
emilyboo <ehbookstein@gmail.com>
2014-10-08 08:16:17 -0700

    Work in progress. Halfway done with the thought of how to pass in changeable data requests

--- Homework6/melon-info.py
+++ Homework6/melon-info.py
@@ -6,11 +6,12 @@ melon_info.py - Prints out all the melons in our inventory
 from melons import melon_data
 
 
+
 def print_melon(data_requests):
 	for request in data_requests:
+		data = melon_data.get(request, "Sorry, that is not a valid key")
 
-
-	for key in melon_data.keys():
+	# for key in melon_data.keys():
 		# if melon_data[key]["seedless"]:
 		# 	hasorhasnot = "doesn't have"
 		# else:
@@ -20,5 +21,8 @@ def print_melon(data_requests):
 
 
 if __name__ == '__main__':
-    for i in melon_data.keys():
-        print_melon(melon_name[i], melon_seedless[i], melon_price[i])
+    data_requests = ["name", "seedless", "price"]
+
+    for key in melon_data.keys():
+        print_melon(data_requests)
+
