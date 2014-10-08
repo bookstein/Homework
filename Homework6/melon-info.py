a6fb9e00e046e2381118065ba4d1e76ffec13698
emilyboo <ehbookstein@gmail.com>
2014-10-08 08:09:52 -0700

    Work in progress, trying to figure out how to allow for easy flexible data requests (e.g. if sales team adds more things to track)

--- Homework6/melon-info.py
+++ Homework6/melon-info.py
@@ -3,18 +3,22 @@ melon_info.py - Prints out all the melons in our inventory
 
 """
 
-from melons import melon_name, melon_seedless, melon_price
+from melons import melon_data
 
 
-def print_melon(name, seedless, price):
-	hashasnot = 'have'
-	if seedless:
-		hashasnot = 'do not have'
+def print_melon(data_requests):
+	for request in data_requests:
 
-	print "%ss %s seeds and are $%0.2f" % ( name, hashasnot, price)
+
+	for key in melon_data.keys():
+		# if melon_data[key]["seedless"]:
+		# 	hasorhasnot = "doesn't have"
+		# else:
+		# 	hasorhasnot= "has"
+		print "%ss %s seeds and are $%0.2f" % (data_requests)
 
 
 
 if __name__ == '__main__':
-    for i in melon_name.keys():
+    for i in melon_data.keys():
         print_melon(melon_name[i], melon_seedless[i], melon_price[i])
