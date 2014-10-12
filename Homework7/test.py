ad78643cb486ca01fcf4fad77e37e44e0f47f4d4
emilyboo <ehbookstein@gmail.com>
2014-10-12 15:05:23 -0700

    Now using for line in iter(f.readline) approach, splitting each line into array, trying to add Called to end of array

--- Homework7/test.py
+++ Homework7/test.py
@@ -3,14 +3,14 @@ def test():
 	header = f.readline().rstrip().split(",") # on first line in file
 	print header
 
-	for line in f:
-		line = f.readline() # starting after first line
+	for line in iter(f.readline,""):
+		# line = f.readline() # starting after first line
 		data = line.rstrip().split(",")
 		# called_or_not gets last item in that array (date of call, or nothing)
 		called_or_not = data[-1]
 		print called_or_not
-		called_or_not = "Called"
-		f.write("".join(data))
+		called_or_not = "Called!!!!"
+		f.write("".join(data)) # this wrote "Called" over the start of every line
 
 
 	# for line in iter(f.readline, ''):
