c9c4e7f52a3e5819aa86afd4c29f2df8acf4ea16
emilyboo <ehbookstein@gmail.com>
2014-10-12 15:02:28 -0700

    Created test python file to try writing to ends of file lines. Persued approach of using f.tell() to figure out where to write Called. Only wrote Called at end of file.

--- Homework7/test.py
+++ Homework7/test.py
@@ -3,24 +3,22 @@ def test():
 	header = f.readline().rstrip().split(",") # on first line in file
 	print header
 
-	line = f.readline() # starting after first line
-	data = line.rstrip().split(",")
-	pos = f.tell()
-	print line
-	print pos
-
-	for line in iter(f.readline, ''):
-		pos = f.tell()
-		# data = line.rstrip().split(",")
-		f.seek(pos)
-		f.write("Called!")
+	for line in f:
+		line = f.readline() # starting after first line
+		data = line.rstrip().split(",")
+		# called_or_not gets last item in that array (date of call, or nothing)
+		called_or_not = data[-1]
+		print called_or_not
+		called_or_not = "Called"
+		f.write("".join(data))
 
 
-	# called_or_not gets last item in that array (date of call, or nothing)
-	# called_or_not = data[-1]
-	# print called_or_not
-	# called_or_not = "Called"
-	# f.write("".join(data))
+	# for line in iter(f.readline, ''):
+	# 	pos = f.tell()
+	# 	# data = line.rstrip().split(",")
+	# 	f.seek(pos)
+	# 	f.write("Called!")
+
 
 
 	# modified = f.read()
